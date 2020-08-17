9b6415d8b18981341e8b680011d89dc0ccafc2fc
phieuvk96 <66987635+phieuvk96@users.noreply.github.com>
2020-08-17 09:11:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,20 +8,4 @@ app.get('/',function(req,res){
 app.listen(port, function() {
   console.log('Listening on port ' + port)
 })
-Trong thư mục ứng dụng mình tạo 1 file demo.html
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Demo Application</title>
-    <link rel="stylesheet" href="/public/css/style.css">
-</head>
-<body>
-<h1> Welcome to your basic web app structure </h1>
-<p>
-    If the title above is red
-    then Nginx is serving static files!
-</p>
-</body>
-</html>
